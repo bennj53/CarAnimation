f9c7c421b91d85c28cba7d5ceb659e1b7d457a0f
JULLIOT Benoît <benoitjulliot@gmail.com>
2020-12-23 00:41:48 +0100

    docs

--- src/app/palm/palm.component.ts
+++ src/app/palm/palm.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from "../../environments/environment";
 
 @Component({
   selector: 'app-palm',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./palm.component.scss']
 })
 export class PalmComponent implements OnInit {
+
+  public prod: boolean = environment.production
 
   constructor() { }
 
